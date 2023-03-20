a8e2be4640a7ec295836b77eb052917cad1c3647
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-20 07:05:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,35 +4,41 @@ ctx = canvas.getContext("2d");
 roverWidth = ?; // definir a largura
 roverHeight = ?; // definir a altura
 
-backgroundImage = "mars.jpg";
+//Coloque o nome da imagem de fundo
+backgroundImage = "???";
 
-roverImage = "rover.png";
+//Coloque o nome da imagem do rover
+roverImage = "???";
 
 roverX = ?; // posição no eixo x
 roverY = ?; // posição no eixo y 
 
 function add() {
-	backgroundImgTag = new Image(); //definindo uma variável com uma nova imagem
+	//Utilize o código 'new Image();', pois a variável 'backgroundImgTag' definirá uma imagem
+	backgroundImgTag = ??? 
 	backgroundImgTag.onload = CarregarPlanoDeFundo; // ajustando uma função, ao carregar essa variável
 	backgroundImgTag.src = backgroundImage;   // carregar a imagem
 
-	roverImgTag = new Image(); //definindo uma variável com uma nova imagem
+	//Utilize o código que definirá a imagem do rover
+	roverImgTag = ??? //definindo uma variável com uma nova imagem
 	roverImgTag.onload = CarregarRover; // ajustando uma função, ao carregar essa variável
 	roverImgTag.src = roverImage;   // carregar a imagem
 }
 
 function CarregarPlanoDeFundo() {
-	ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
+	//Utilize 'drawImage' para que a imagem especificada seja desenhada na tela
+	ctx.???(backgroundImgTag, 0, 0, canvas.width, canvas.height);
 }
 
 function CarregarRover() {
-	ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight);
+	ctx.???(roverImgTag, roverX, roverY, roverWidth, roverHeight);
 }
 
+//Utilize 'addEventListener', pois se o evento que acontecer na tela for 'pressionar tecla para baixo', acontecerá a função 'myKeyDown'
+window.???("keydown", myKeyDown);
 
-window.addEventListener("keydown", myKeyDown);
-
-function myKeyDown(e)
+//Digite o nome da função
+function ???(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
@@ -61,7 +67,8 @@ function cima()
 {
 	if(roverY >=0)
 	{
-		roverY = roverY - 10;
+		//Coloque o nome da variável que gaurda o eixo y do rover
+		??? = ??? - 10;
 		CarregarPlanoDeFundo();
 		 CarregarRover();
 	}
@@ -70,7 +77,8 @@ function baixo()
 {
 	if(roverY <=500)
 	{
-		roverY =roverY+ 10;
+		//Coloque o nome da variável que gaurda o eixo y do rover
+		??? =???+ 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
 	}
@@ -79,7 +87,8 @@ function esquerda()
 {
 	if(roverX >= 0)
 	{
-		roverX =roverX - 10;
+		//Coloque o nome da variável que gaurda o eixo X do rover
+		??? =??? - 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
 	}
@@ -88,7 +97,8 @@ function direita()
 {
 	if(roverX <= 700)
 	{
-		roverX =roverX + 10;
+		//Coloque o nome da variável que gaurda o eixo X do rover
+		??? =??? + 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
    }
